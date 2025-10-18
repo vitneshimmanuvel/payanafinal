9516f1b8ab56565e852076f40f95a84ef4f1d6e0
Vitnesh Immanuvel <vitneshimmanuvel@gmail.com>
2025-10-18 16:35:09 +0530

    nav links for the study

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import './App.css'
 import BannerSection from './components/Baneer'
 import DentalProgram from './components/DentalProgram'
@@ -14,18 +15,49 @@ import Techarts from './components/Techarts'
 import TestimonialSection from './components/TestimonialSection'
 import WhatsAppButton from './components/WhatsAppButton'
 import AdPopup from './components/AdPopup '
+
 function App() {
+  
+  // Auto-scroll to section when page loads with hash in URL
+  useEffect(() => {
+    const scrollToHashElement = () => {
+      const { hash } = window.location;
+      
+      if (!hash) return;
+      
+      const elementToScroll = document.getElementById(hash.replace("#", ""));
+
+      if (!elementToScroll) return;
+
+      setTimeout(() => {
+        window.scrollTo({
+          top: elementToScroll.offsetTop - 80,
+          behavior: "smooth"
+        });
+      }, 100);
+    };
+
+    scrollToHashElement();
+    
+    // Listen for hash changes
+    window.addEventListener("hashchange", scrollToHashElement);
+    return () => window.removeEventListener("hashchange", scrollToHashElement);
+  }, []);
+
   return (
     <div className='total'>
        <AdPopup />
       <Navbartop />
       <BannerSection />
+      
       <div id='tech'>
         <Techarts />
       </div>
+      
       <div id='mbbs'>
         <MBBSInfo />
       </div>
+      
       <div id='study'>
         <StudyOptions/>
       </div>
@@ -35,12 +67,13 @@ function App() {
       </div>
            
       <div id='work'>
-      <HelpCommunity />
+        <HelpCommunity />
       </div>
       
       <div id='invest'>
         <Invest/>
       </div>
+      
       <NewsGPT />
       <TestimonialSection />
       <Footer />
